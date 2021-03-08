2a299858c43ac15ad07deb41ac10b697ad550894
Mihail Hadzhiev <mihail.hadzhiev@rewardgateway.com>
2021-03-08 09:59:24 +0200

    unite testing

--- test_string_math.py
+++ test_string_math.py
@@ -1,5 +1,5 @@
 import math_func
-
+import pytest
 
 def test_add_strings():
     result = math_func.add('Hello ', 'Mihail')
@@ -10,3 +10,18 @@ def test_add_strings():
 def test_product_string():
     result = math_func.product('Hello ')
     assert result == 'Hello Mihail'
+    assert type(result) == str
+    assert 'Hello' in result
+
+
+def test_add():
+    assert math_func.add(7, 3) == 10
+    assert math_func.add(8) == 10
+    assert math_func.add(5, 5) == 10
+
+
+def test_add():
+    assert math_func.add(7, 3) == 10
+    assert math_func.add(8) == 10
+    assert math_func.add(5, 5) == 10
+
