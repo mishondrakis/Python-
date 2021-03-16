b999c6c61eddb8b497ee883ef6d2eca4264a0412
Mihail Hadzhiev <mihail.hadzhiev@rewardgateway.com>
2021-03-16 07:48:56 +0200

    List and Tuples

--- List_Tuples/List.py
+++ List_Tuples/List.py
@@ -5,3 +5,17 @@ print(rowCars[0])
 rowCars[1] = 'Iskam'
 print(rowCars[1])
 print(rowCars)
+rowCars.append('Honda')
+print(rowCars)
+rowCars.remove("Nissan")
+if 'Nissan' in rowCars:
+    rowCars.remove('Nissan')
+
+print(rowCars)
+rowCars.sort()
+print(rowCars)
+
+rowCars.insert(1,'Tesla')
+print(rowCars.index('Tesla'))
+rowCars.pop(1)
+print(rowCars)
---
+++ List_Tuples/MultiDimensional lists.py
@@ -0,0 +1,10 @@
+cars = [['Toyota', 'Honda', 'Nissan'],
+        ['Porsche', 'Honda', 'Tesla'],
+        ['Lada', 'Trabant']]
+
+my_favorite_car = cars[1][0]
+print(my_favorite_car)
+
+for floor in cars:
+    for row in floor:
+        print(row)
